214756e8382eb76a4b301e70c7ae7692e2402177
Paul Belanger <pabelanger@redhat.com>
2019-03-13 21:09:24 -0400

    Manage bastion SSH known_hosts file
    This attempts to manage the SSH host keys of all the hosts in the
    inventory. The idea here is each host in the inventory will add a
    ansible_ssh_host_key_ecdsa_public variable, then when we first run our
    bastion playbook we'll properly populate the local known_hosts file each
    time.
    
    Change-Id: I26e328192a7127086e514dc62a27cb946a77440b
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- launch/utils.py
+++ launch/utils.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import errno
 import socket
 import time
 
@@ -30,6 +31,55 @@ def iterate_timeout(max_seconds, purpose):
         yield count
         time.sleep(2)
     raise Exception("Timeout waiting for %s" % purpose)
+
+
+def nodescan(ip, port=22, timeout=60):
+    '''
+    Scan the IP address for public SSH keys.
+
+    Keys are returned formatted as: "<type> <base64_string>"
+    '''
+    addrinfo = socket.getaddrinfo(ip, port)[0]
+    family = addrinfo[0]
+    sockaddr = addrinfo[4]
+
+    ret = None
+    key = None
+    for count in iterate_timeout(
+            timeout, "connection to %s on port %s" % (ip, port)):
+        sock = None
+        t = None
+        try:
+            sock = socket.socket(family, socket.SOCK_STREAM)
+            sock.settimeout(10)
+            sock.connect(sockaddr)
+            t = paramiko.transport.Transport(sock)
+            t._preferred_keys = ['ssh-ed25519']
+            t.start_client(timeout=timeout)
+            key = t.get_remote_server_key()
+            break
+        except socket.error as e:
+            if e.errno not in [errno.ECONNREFUSED, errno.EHOSTUNREACH, None]:
+                log.exception(
+                    'Exception connecting to %s on port %s:' % (ip, port))
+        except Exception as e:
+            log.exception("ssh-keyscan failure: %s", e)
+        finally:
+            try:
+                if t:
+                    t.close()
+            except Exception as e:
+                log.exception('Exception closing paramiko: %s', e)
+            try:
+                if sock:
+                    sock.close()
+            except Exception as e:
+                log.exception('Exception closing socket: %s', e)
+
+    if key:
+        ret = key.get_base64()
+
+    return ret
 
 
 def ssh_connect(ip, username, connect_kwargs={}, timeout=60):
